718dcba960db2fbdab08ce1e57fff05f898d1a0d
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-05-03 19:54:05 -0400

    feat: :sparkles: implement basic Vector
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- vector/src/lib.rs
+++ vector/src/lib.rs
@@ -1,5 +1,12 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, PartialEq, Eq, Default)]
+pub struct Vector<T> {
+    data: Vec<T>,
+}
+
+impl<T> From<Vec<T>> for Vector<T> {
+    fn from(value: Vec<T>) -> Self {
+        Vector { data: value }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +14,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn define_empty_vector() {
+        let result: Vector<bool> = Vector::<bool>::default();
+        assert_eq!(result, Vector { data: vec![] });
+    }
+
+    #[test]
+    fn create_vector_from_vec() {
+        let result = Vector::from(vec![1]);
+        assert_eq!(result, Vector { data: vec![1] });
+    }
+
+    #[test]
+    fn transform_vec_into_vector() {
+        let result: Vector<i32> = vec![1].into();
+        assert_eq!(result, Vector { data: vec![1] });
     }
 }
