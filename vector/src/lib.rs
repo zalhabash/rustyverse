e8380b6239b0499c0ce0b2adb29d3fb6ecaa4365
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-05-03 21:43:44 -0400

    feat: :sparkles: implement basic vectorized addition
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- vector/src/lib.rs
+++ vector/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 #[derive(Debug, PartialEq, Eq, Default)]
 pub struct Vector<T> {
     data: Vec<T>,
@@ -6,6 +8,34 @@ pub struct Vector<T> {
 impl<T> From<Vec<T>> for Vector<T> {
     fn from(value: Vec<T>) -> Self {
         Vector { data: value }
+    }
+}
+
+impl<T, Rhs> Add<Rhs> for Vector<T>
+where
+    T: Add<Rhs>,
+    Rhs: Copy,
+{
+    type Output = Vector<<T as Add<Rhs>>::Output>;
+
+    fn add(self, rhs: Rhs) -> Self::Output {
+        Vector {
+            data: self.data.into_iter().map(|value| value + rhs).collect(),
+        }
+    }
+}
+
+impl<'a, T, Rhs> Add<Rhs> for &'a Vector<T>
+where
+    &'a T: Add<Rhs>,
+    Rhs: Copy,
+{
+    type Output = Vector<<&'a T as Add<Rhs>>::Output>;
+
+    fn add(self, rhs: Rhs) -> Self::Output {
+        Vector {
+            data: self.data.iter().map(|value| value + rhs).collect(),
+        }
     }
 }
 
@@ -29,5 +59,41 @@ mod tests {
     fn transform_vec_into_vector() {
         let result: Vector<i32> = vec![1].into();
         assert_eq!(result, Vector { data: vec![1] });
+    }
+
+    #[test]
+    fn add_to_ref_of_vector() {
+        let result = &Vector::from(vec![1, 2]) + 1;
+        assert_eq!(result, Vector::from(vec![2, 3]))
+    }
+
+    #[test]
+    fn add_to_vector() {
+        let result = Vector::from(vec![1, 2]) + 1;
+        assert_eq!(result, Vector::from(vec![2, 3]))
+    }
+
+    #[test]
+    fn add_ref_to_ref_vector() {
+        let result = &Vector::from(vec![1, 2]) + &1;
+        assert_eq!(result, Vector::from(vec![2, 3]))
+    }
+
+    #[test]
+    fn add_ref_to_vector() {
+        let result = Vector::from(vec![1, 2]) + &1;
+        assert_eq!(result, Vector::from(vec![2, 3]))
+    }
+
+    #[test]
+    fn add_str_to_vector_of_strings() {
+        let actual = Vector::from(vec!["Hello".to_string(), "Goodbye".to_string()]) + " world!";
+        assert_eq!(
+            Vector::from(vec![
+                "Hello world!".to_string(),
+                "Goodbye world!".to_string()
+            ]),
+            actual
+        )
     }
 }
